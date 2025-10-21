a9f6e9eb6a3d79d98a0ab1fe96e4e0ccc8528447
Tatasel Daniel <daniel.tatasel@yahoo.com>
2025-10-22 01:46:29 +0300

    Adaugat la tema 3

---
+++ OOP_Kotlin/tema3/lab3_2.kt
@@ -0,0 +1,17 @@
+class Student(){
+
+    private val nume: String=""
+    private   val prenume: String=""
+    private   val CNP:String=""
+    private  val anNastere: Int=0
+    private  val numeFac:String=""
+    private   val anInfiintare:Int=0
+
+    fun genCNP():String{
+
+    }
+
+
+
+
+}
