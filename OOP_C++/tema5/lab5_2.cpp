348aee5cb115f99ad40dacecdd3c5510de0e8096
Tatasel Daniel <daniel.tatasel@yahoo.com>
2025-11-08 17:59:57 +0200

    Added tema 5 C++ modifications

--- OOP_C++/tema5/lab5_2.cpp
+++ OOP_C++/tema5/lab5_2.cpp
@@ -3,53 +3,63 @@
 using namespace std;
 
 class CEO{
+protected:
+    string nume;
+    double salariu;
 public:
- void salary(){
-     cout<<"10M per year"<<endl;
+    CEO(string n = "Jeff Bezos", double s = 10) : nume(n), salariu(s) {}
+ virtual void salary(){
+     cout<<salariu<<"M per year"<<endl;
    }
-    void name() {
-     cout<<"Jeff Bezos"<<endl;
+    virtual void name() {
+     cout<<nume<<endl;
  }
 };
 
 class Manager:public CEO{
 public:
-    void salary() {
-        cout<<"Could buy a Porsche, but drives a Prius instead (around 100k a year)"<<endl;
+    Manager(string n = "Mark Henry", double s= 100) : CEO(n, s) {}
+    void salary() override{
+        cout<<"Could buy a Porsche, but drives a Prius instead "<<"(around "<<salariu<<"k a year)"<<endl;
     }
 
-    void name() {
-        cout<<"Mark Henry"<<endl;
+    void name() override{
+        cout<<nume<<endl;
     }
 
 };
 
 class Developer:public Manager{
     public:
-    void salary() {
-        cout<<"Loans a Porsche to drive around, lying to himself he will buy it someday (around 60k a year)"<<endl;
+    Developer(string n = "Matt", double s = 50) : Manager(n, s) {}
 
+    void salary() override{
+        cout<<"Loans a Porsche to drive around, lying to himself he will buy it someday"<<"(around "<<salariu<<"k a year)"<<endl;
     }
 
-    void name() {
-        cout<<"Matt"<<endl;
+    void name() override{
+        cout<<nume<<endl;
     }
 
 };
 
 
 int main(){
+    Developer dev;
+    Manager manager;
     CEO ceo;
-    Manager m;
-    Developer d;
 
-    ceo.name();
-    ceo.salary();
-    cout<<"\n";
-    m.name();
-    m.salary();
-    cout<<"\n";
-    d.name();
-    d.salary();
+    CEO* p1=&ceo;
+    CEO* p2=&manager;
+    CEO* p3=&dev;
+
+    p1->name();
+    p1->salary();
+
+    p2->name();
+    p2->salary();
+
+    p3->name();
+    p3->salary();
     return 0;
 }
