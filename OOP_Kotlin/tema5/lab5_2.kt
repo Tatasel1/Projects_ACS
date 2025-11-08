d0b0685a0ddf5584a89b25c0df5f904ac531bf4d
Tatasel Daniel <daniel.tatasel@yahoo.com>
2025-11-08 18:06:45 +0200

    added tema 5 modifications

--- OOP_Kotlin/tema5/lab5_2.kt
+++ OOP_Kotlin/tema5/lab5_2.kt
@@ -1,42 +1,47 @@
-open class CEO{
-    open fun salary(){
-        println("10M per year")
+open class CEO(
+    open val nume: String = "Jeff Bezos",
+    open val salariu: Double = 10.0
+) {
+    open fun salary() {
+        kotlin.io.println("${salariu}M per year")
     }
-    open fun name(){
-        println("Jeff Bezos")
+
+    open fun name() {
+        kotlin.io.println(nume)
     }
 }
 
-
-open class Manager: CEO(){
-    open override fun salary(){
-        println("Could buy a Porsche, but drives a Prius instead (around 100k a year)")
-    }
-    open override fun name(){
-        println("Mark Henry")
+open class Manager(
+    override val nume: String = "Mark Henry",
+    override val salariu: Double = 100.0
+) : CEO(nume, salariu) {
+    override fun salary() {
+        kotlin.io.println("Could buy a Porsche, but drives a Prius instead (around ${salariu}k a year)")
     }
 }
 
-class Developer: Manager(){
-    override fun salary(){
-        println("Loans a Porsche to drive around, lying to himself he will buy it someday (around 60k a year)")
-    }
-
-    override fun name(){
-        println("Matt")
+class Developer(
+    override val nume: String = "Matt",
+    override val salariu: Double = 50.0
+) : Manager(nume, salariu) {
+    override fun salary() {
+        kotlin.io.println("Loans a Porsche to drive around, lying to himself he will buy it someday (around ${salariu}k a year)")
     }
 }
 
 fun main(){
     val ceo=CEO();
-    ceo.salary()
     ceo.name()
+    ceo.salary()
+
 
     val manager=Manager()
-    manager.salary()
     manager.name()
+    manager.salary()
+
 
     val developer=Developer()
-    developer.salary()
     developer.name()
+    developer.salary()
+
 }
