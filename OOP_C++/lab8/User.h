6984f3652c553433706ffcef3df91ebccf04215d
Tatasel Daniel <daniel.tatasel@yahoo.com>
2025-12-03 02:17:11 +0200

    lab8 first change

---
+++ OOP_C++/lab8/User.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <string>
+
+class IUser {
+    public:
+    virtual void print() = 0;
+    virtual ~IUser() = default;
+};
+
+class User: public IUser {
+    protected:
+    std::string name;
+    std::string email;
+    std::string password;
+
+    public:
+    void print() override;
+    ~Operator() override = default;
+};
+
+class Operator: public User {
+    public:
+    void addCursa();
+    void removeCursa();
+    void print() override;
+    ~Operator() override = default;
+};
+
+class Client: public User {
+    public:
+    void createAccount();
+    void bookCursa();
+    void cancelCursa();
+    void print() override;
+    ~Client() override = default;
+};
+
+///class Cursa
